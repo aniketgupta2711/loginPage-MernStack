dd9bca05abd8906224da70f2a317f84ee2725117
Aniket Gupta <omgrealme962@gmail.com>
2026-08-03 01:10:44 +0530

    Update signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,26 +1,70 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {ToastContainer} from 'react-toastify';
 
 function Signup() {
+
+  const [signupInfo, setSignupInfo] = useState({
+    name: '',
+    email:'',
+    password:'',
+
+  })
+
+  const handleChange = (e) => {
+     const {name, value} = e.target;
+     console.log(name, value);
+     const  copySignupInfo = { ...signupInfo };
+     copySignupInfo[name] = value;
+     setSignupInfo(copySignupInfo);
+  }
+  console.log('loginInfo -> ', loginInfo)
+  const handleSignup = (e) => {
+    e.preventDefault();
+  }
+
   return (
+
     <div className='container'>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSignup}>
+
          <div>
           <label htmlFor='name'>Name</label>
-          <input type="text" name='name' autoFocus placeholder='Enter your name...' />
+          <input
+          onChange={handleChange}
+           type="text"  
+           name='name' 
+           autoFocus 
+           placeholder='Enter your name...' 
+           value={setSignupInfo.name}
+           />
          </div>
+
          <div>
           <label htmlFor='email'>Email</label>
-          <input type="email" name='email' autoFocus placeholder='Enter your email...' />
+          <input
+          onChange={handleChange}
+          type="email" 
+          name='email' 
+          placeholder='Enter your email...' 
+          value={setSignupInfo.email}
+          />
          </div>
+
          <div>
           <label htmlFor='password'>Password</label>
-          <input type="password" name='password' autoFocus placeholder='Enter your password...' />
+          <input 
+          onChange={handleChange}
+          type="password" 
+          name='password' 
+          placeholder='Enter your password...' 
+          value={setSignupInfo.password}
+        />
          </div>
-         <button>Signup</button>
-         <span>Already have an account ?
+         
+         <button type='submit'>Signup</button>
+         <span> Already have an account ?
           <Link to="/login">Login</Link>
          </span>
 
@@ -31,3 +75,4 @@ function Signup() {
 }
 
 export default Signup
+
